c42b443cf8208fe151c35d0b4be848c26e54b8b4
Snehal Gondaliya <sgondaliya92@gmail.com>
2024-02-12 20:32:03 -0500

    change cors origin

--- backend/soket/socket.js
+++ backend/soket/socket.js
@@ -7,9 +7,8 @@ const app = express()
 const server = http.createServer(app)
 const io = new Server(server,{
     cors:{
-        origin:["http://localhost:8000"],
-        methods: ["GET", "POST"],
-        credentials:true
+        origin:"*",
+        methods: ["GET", "POST"]
     }
 })
 
